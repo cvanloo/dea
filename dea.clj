55bcb6c66dd4130e7fbf56e468f91898b6700415
Colin van Loo <colin.vanloo@ost.ch>
2024-04-27 14:57:48 +0200

    add even dea

--- dea.clj
+++ dea.clj
@@ -18,6 +18,16 @@
    :start \V
    :accept #{\V}})
 
+(def dea-even
+  {:states #{\E \O}
+   :alphabet #{\0 \1}
+   :transitions #{[\E \0 \E]
+                  [\E \1 \O]
+                  [\O \0 \E]
+                  [\O \1 \O]}
+   :start \E
+   :accept #{\E}})
+
 (defn run-dea
   [{:keys [states alphabet transitions start accept]} input]
   (letfn [(find-transition [s1 c]
@@ -28,8 +38,14 @@
               (recur (last (find-transition state c)) input)))]
     (step start input)))
 
-; user=> (dea/-main "" "D" "DF" "DFFF" "DFFFD")
+; user=> (dea/-main "drehkreuz" "" "D" "DF" "DFFF" "DFFFD")
 ; ([V true] [V true] [E false] [E false] [V true])
+;
+; (dea/-main "even" "" "0" "1" "01" "010")
+; ([E true] [E true] [O false] [O false] [E true])
 (defn -main
-  [& args]
-  (println (map (partial run-dea dea-drehkreuz) args)))
+  [dea & args]
+  (let [deas
+        {"even" dea-even
+         "drehkreuz" dea-drehkreuz}]
+    (println (map (partial run-dea (get deas dea)) args))))
